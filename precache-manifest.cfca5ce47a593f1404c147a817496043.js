5ab2051af2fd56b2738fa173cfb9908c40fc2c9b
VSPaul <vascupaul_vsp@yahoo.com>
2019-10-28 11:54:58 +0200

    Updates

---
+++ precache-manifest.cfca5ce47a593f1404c147a817496043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df84afe69a758fc93b45240af6d9322",
+    "url": "/my-travels/index.html"
+  },
+  {
+    "revision": "2cd256f4c71daf42846a",
+    "url": "/my-travels/static/css/main.0c47a8f2.chunk.css"
+  },
+  {
+    "revision": "0dd14582a321d103ba3a",
+    "url": "/my-travels/static/js/2.494abc18.chunk.js"
+  },
+  {
+    "revision": "2cd256f4c71daf42846a",
+    "url": "/my-travels/static/js/main.58f8df2d.chunk.js"
+  },
+  {
+    "revision": "a7494290411757e23a1e",
+    "url": "/my-travels/static/js/runtime-main.79b16804.js"
+  },
+  {
+    "revision": "5f066de47ebc6dff5daabb71f22d40a3",
+    "url": "/my-travels/static/media/cocktail-svgrepo-com.5f066de4.svg"
+  }
+]);
